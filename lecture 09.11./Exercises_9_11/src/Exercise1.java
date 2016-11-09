955183aca89a3c2c98841ae43ae8c1b54026585d
Julian <julian.lemmel@gmail.com>
2016-11-09 14:14:51 +0100

    lecture 9.11.

---
+++ lecture 09.11./Exercises_9_11/src/Exercise1.java
@@ -0,0 +1,21 @@
+/**
+ * Exercise1)
+ * implement the method 'sum' using recurion to calculate the sum of all number from 0 to the given parameter n
+ */
+public class Exercise1 {
+
+        public static void main(String[] args) throws Exception{
+
+            //You can use the main method to test your solution
+                up(4);
+        }
+
+        /*
+        Prints out all numbers from 0 to n using recursion.
+         */
+        private static void up(int n) throws Exception{
+
+            //TODO implement here
+
+        }
+}
---
+++ lecture 09.11./Exercises_9_11/src/Exercise5.java
@@ -0,0 +1,33 @@
+/*
+    Exercise5)
+
+    Implement a function called 'drawDiamond' taking a parameter int called h.
+    This function should print a diamond like this, where h is the 'height' of the diamond, so for e.g. h=9:
+        1
+       222
+      33333
+     4444444
+    555555555
+     4444444
+      33333
+       222
+        1
+     You are going to need odd numbers to do this, so just print 'No valid input!' if an even number or a number smaller
+     than 0 is given. In case of 0 just leave the function right away without printing anything.
+
+
+     This time try to avoid loops whenever possible!
+     (it's hard, but possible, to solve this exercise without using any loops!)
+     You will certainly have to write additional functions.
+
+*/
+public class Exercise5 {
+
+    public static void main(String[] args) {
+        //use this method to test your function
+    }
+
+    public static void drawDiamond(int h){
+        //...
+    }
+}
