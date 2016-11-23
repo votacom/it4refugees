53eb33e2ce7e98fb75d50415338038a622d3e286
Alex Hauer <alex.hauer@happyninja.net>
2016-11-23 16:35:29 +0100

    exercises - Metrics, StringToArray

---
+++ lecture 23.11./exercises/src/Metrics.java
@@ -0,0 +1,19 @@
+/**
+ * Write a function that computes the sum of numbers in an array,
+ * the average of the numbers in the array,
+ * and returns a new array with the computed sum and average as its elements.
+ */
+public class Metrics {
+
+    public static void main(String[] args) {
+        int[] numbers1 = {1, 2, 3, 4, 5};
+        int[] metrics1 = getMetrics(numbers1); // should return {15, 3}
+
+        int[] numbers2 = {5, 10, 15, 20, 25};
+        int[] metrics2 = getMetrics(numbers2); // should return {80, 16}
+    }
+
+    public static int[] getMetrics(int[] numbers) {
+        return null;
+    }
+}
