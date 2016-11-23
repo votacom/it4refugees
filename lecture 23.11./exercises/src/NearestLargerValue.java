cbe78a4ae826bc00e6fdb4406c47f2edc2950a0c
Alex Hauer <alex.hauer@happyninja.net>
2016-11-23 16:24:43 +0100

    exercises - NearestLargerValue

---
+++ lecture 23.11./exercises/src/NearestLargerValue.java
@@ -0,0 +1,20 @@
+/**
+ * Write a function that takes an array of numbers and a number as input parameters,
+ * and returns the index of the next greater number.
+ *
+ * For example, for the array [1,4,3,2,5,7] and the number 4, the function should
+ * return 4 (the index of the next greater number, 5). Consequently, for the same
+ * array and the number 5, the function should return 6 (the index of 7).
+ *
+ * If you want, you can also write another function that returns the "distance"
+ * to the next index, that is, how many indices after the given number the next
+ * greater number is stored. For the examples above, for input 4, the function
+ * should return 3, for input 5, the function should return 1.
+ */
+public class NearestLargerValue {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
