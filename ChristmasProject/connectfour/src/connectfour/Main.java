059187ebdf8c0185b899819dd229e311b0e61c29
Julian <julian@e253-166.eduroam.tuwien.ac.at>
2016-12-14 16:52:41 +0100

    lecture 14.12.

---
+++ ChristmasProject/connectfour/src/connectfour/Main.java
@@ -0,0 +1,8 @@
+package connectfour;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO: Create an instance of Game and run the game
+    }
+}
---
+++ ChristmasProject/connectfour/src/connectfour/TileColor.java
@@ -0,0 +1,18 @@
+package connectfour;
+
+/**
+ * Enumeration representing the color of a tile. Can be either Red or Blue.
+ */
+public enum TileColor {
+    Red("X"), Blue("O");
+
+    TileColor(String str) {
+        // TODO: store the string representation in a property so it can be returned by toString()
+    }
+
+    @Override
+    public String toString() {
+        // TODO: return the correct string representation here
+        return "";
+    }
+}
